3feb6cd7de846e860dc489cbaaf3d1be06492869
lisoboss <37949544+lisoboss@users.noreply.github.com>
2025-12-22 17:30:25 +0800

    init .

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for controlflowutils."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """lisoboss"""
+__email__ = "37949544+lisoboss@users.noreply.github.com"
+__version__ = "0.0.1"
+
+from .src.controlflowutils.nodes import NODE_CLASS_MAPPINGS
+from .src.controlflowutils.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
